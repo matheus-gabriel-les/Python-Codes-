dd12867526bde18048f11ff85966faf05e0835bc
Matheus Gabriel Lino <100940909+matheus-gabriel-les@users.noreply.github.com>
2026-02-25 20:01:04 -0300

    Refactor random number generation logic

--- Number.py
+++ Number.py
@@ -1,16 +1,14 @@
 import random
 numero_escolhido = ""
-MED = 500
-AUX = 0
+MED = random.randint(1,5000)
 count = 0
 while numero_escolhido!= "sim":
     tentativa = input("O numero escolido e maior que "+str(MED)+ "?")
     if tentativa == "sim":
-        AUX = MED + 1
-        MED = random.randint(MED,AUX)
+        MED = random.randint(MED,MED*2)
+        numero_escolhido = input("O numero escolido e "+str(MED)+"?")
     elif tentativa == "nao":
-        MED = MED - 1
-        MED = random.randint(AUX,MED)
-    numero_escolhido = input("O numero escolido e "+str(MED)+"?")
+        MED = random.randint(MED//2,MED)
+        numero_escolhido = input("O numero escolido e "+str(MED)+"?")
     count += 1
 print("O numero escolhido e "+str(MED)+" e o numero de tentativas foi: "+str(count))  

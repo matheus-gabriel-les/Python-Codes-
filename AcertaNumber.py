4980f6aae4910ac8867c1e5022bd7f3ed151c246
Matheus Gabriel Lino <100940909+matheus-gabriel-les@users.noreply.github.com>
2026-05-25 15:12:49 -0300

    Refactor guessing game with functions
    Refactor number guessing game to use functions for better structure and readability.

--- AcertaNumber.py
+++ AcertaNumber.py
@@ -1,16 +1,46 @@
-low, high = 1, 5000
-print(f"Pense em um número entre {low} e {high}. Responda às perguntas com 'maior', 'menor' ou 'sim'.")
-count = 0
-while low <= high:
-    mid = (low + high) // 2
-    resposta = input(f"Seu número é {mid}? ").strip().lower()
-    count += 1
-    if resposta in ("sim", "s", "igual"):
-        print(f"Encontrei: {mid} em {count} tentativas.")
-        break
-    if resposta in ("maior", ">"):
-        low = mid + 1
-        continue
-    if resposta in ("menor", "<"):
-        high = mid - 1
-        continue
+def pergunta_sim_nao(prompt):
+    while True:
+        resposta = input(prompt).strip().lower()
+        if resposta in ("sim"):
+            return True
+        if resposta in ("nao"):
+            return False
+        print("Por favor, responda com 'sim' ou 'nao' .")
+
+def maior_que(valor):
+    return pergunta_sim_nao(f"O número escolhido é MAIOR que {valor}? ")
+
+def encontrar_intervalo():
+    # Pergunta base para definir a direção (positivos ou negativos)
+    if maior_que(0):
+        low, high = 0, 10
+        while maior_que(high):
+            low = high
+            high *= 10
+        return low, high
+    else:
+        high, low = 0, -10
+        while not maior_que(low):
+            high = low
+            low *= 10
+        return low, high
+#Implementacao de algoritimo de busca binaria
+def busca_binaria():
+    low, high = encontrar_intervalo()
+    while low + 1 < high:
+        mid = (low + high) // 2 
+        
+        if maior_que(mid):
+            low = mid
+        else:
+            high = mid
+            
+    return high
+
+def main():
+    print("Pense em um número inteiro. Eu vou tentar adivinhá-lo usando perguntas.")
+    numero = busca_binaria()
+    print(f"\nJá sei! O número escolhido é {numero}.")
+
+if __name__ == "__main__":
+    main()
